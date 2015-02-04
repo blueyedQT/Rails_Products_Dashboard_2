9108a25c379f0a9b18ed44614a0f6fa43b84a771
Katrina Sanford <blueyed_qt@hotmail.com>
2015-02-04 14:17:11 -0800

    Added comment model

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,7 @@
 class Product < ActiveRecord::Base
 	belongs_to :category
+	has_many :comments
+	
 	validates :category, :name, :description, presence: true
 	validates :pricing, presence: true, numericality: { greater_than: 0 }
 end
